83269ae501f00005a74c00bec341ee8f9f256b02
Jesper Andersson <jsprA86@gmail.com>
2015-03-03 22:15:52 +0100

    Inte alls färdig

--- A5_Jesper_Andersson/src/ClockThread.java
+++ A5_Jesper_Andersson/src/ClockThread.java
@@ -1,14 +1,27 @@
+import java.util.Calendar;
 
 public class ClockThread extends Thread{
 	
 	private ClockInterface clockInterface;
+	private Calendar calendar;
 	
 	public ClockThread(ClockInterface cI){
 		clockInterface = cI;
 	}
 	
+	
+	//fungerar inte ännu, fortsätt här imorgon.
 	public void run(){
-		
+		while(true){
+			calendar.get(0);
+			clockInterface.update(0,0,0);
+			try {
+				Thread.sleep(900);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 	
 }
