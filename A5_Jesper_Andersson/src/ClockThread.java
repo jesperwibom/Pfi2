5fc5a7eeb4bf4a5e1afe7ced51225f94a219c7ec
Jesper Andersson <jsprA86@gmail.com>
2015-03-04 20:02:09 +0100

    Började planera upp hur jag ska bygga GUI't

--- A5_Jesper_Andersson/src/ClockThread.java
+++ A5_Jesper_Andersson/src/ClockThread.java
@@ -3,22 +3,18 @@ import java.util.Calendar;
 public class ClockThread extends Thread{
 	
 	private ClockInterface clockInterface;
-	private Calendar calendar;
 	
 	public ClockThread(ClockInterface cI){
 		clockInterface = cI;
 	}
 	
-	
-	//fungerar inte ännu, fortsätt här imorgon.
 	public void run(){
 		while(true){
-			calendar.get(0);
-			clockInterface.update(0,0,0);
 			try {
+				Calendar calendar = Calendar.getInstance();
+				clockInterface.update(calendar.get(Calendar.HOUR_OF_DAY),calendar.get(Calendar.MINUTE),calendar.get(Calendar.SECOND));
 				Thread.sleep(900);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
--- A5_Jesper_Andersson/src/DigitalClockGUI.java
+++ A5_Jesper_Andersson/src/DigitalClockGUI.java
@@ -1,13 +1,33 @@
+
+import java.awt.BorderLayout;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 
 public class DigitalClockGUI extends JFrame {
 	
+	private JPanel clockPanel;
+	private JTextArea clockDisplay;
+	private JPanel buttonPanel;
+	
+	private JLabel lblHours;
+	private JTextArea txtHours;
+	private JLabel lblMinutes;
+	private JTextArea txtMinutes;
+	private JButton btnSetAlarm;
+	private JButton btnCancelAlarm;
+	
 	private ClockLogic clockLogic;
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 	
 	public DigitalClockGUI(){
