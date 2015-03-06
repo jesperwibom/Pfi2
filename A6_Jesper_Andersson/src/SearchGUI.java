21aab3fa7dbfcaef4fdb24476aa9bf6ac80e5d04
Jesper Andersson <jsprA86@gmail.com>
2015-03-06 12:20:10 +0100

    Startade med thread

--- A6_Jesper_Andersson/src/SearchGUI.java
+++ A6_Jesper_Andersson/src/SearchGUI.java
@@ -8,12 +8,17 @@ import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JTextArea;
 
+import se.mah.k3lara.skaneAPI.control.Constants;
+import se.mah.k3lara.skaneAPI.model.Journey;
+import se.mah.k3lara.skaneAPI.model.Journeys;
 import se.mah.k3lara.skaneAPI.model.Station;
 import se.mah.k3lara.skaneAPI.xmlparser.Parser;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.util.ArrayList;
+import java.util.Calendar;
+
 import javax.swing.JScrollPane;
 
 
@@ -22,6 +27,9 @@ public class SearchGUI extends JFrame {
 	private JPanel contentPane;
 	private JTextField txtSearch;
 	JTextArea txtrResult = new JTextArea();
+	JTextArea txtrResult_1 = new JTextArea();
+	private JTextField txtFrom;
+	private JTextField txtTo;
 	
 	/**
 	 * Launch the application.
@@ -44,19 +52,19 @@ public class SearchGUI extends JFrame {
 	 */
 	public SearchGUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 346);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JPanel panel = new JPanel();
-		panel.setBounds(6, 6, 438, 160);
+		panel.setBounds(6, 6, 438, 165);
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
 		txtSearch = new JTextField();
-		txtSearch.setText("Search");
+		txtSearch.setText("SearchStation");
 		txtSearch.setBounds(76, 6, 134, 28);
 		panel.add(txtSearch);
 		txtSearch.setColumns(10);
@@ -81,8 +89,80 @@ public class SearchGUI extends JFrame {
 		
 		
 		txtrResult.setText("Result");
-		txtrResult.setBounds(6, 37, 426, 117);
+		txtrResult.setBounds(6, 37, 426, 122);
 		panel.add(txtrResult);
 		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(6, 178, 438, 140);
+		contentPane.add(panel_1);
+		panel_1.setLayout(null);
+		
+		txtFrom = new JTextField();
+		txtFrom.setText("From");
+		txtFrom.setBounds(6, 6, 134, 28);
+		panel_1.add(txtFrom);
+		txtFrom.setColumns(10);
+		
+		txtTo = new JTextField();
+		txtTo.setText("To");
+		txtTo.setBounds(152, 6, 134, 28);
+		panel_1.add(txtTo);
+		txtTo.setColumns(10);
+		
+		JButton btnSearch_1 = new JButton("Search");
+		btnSearch_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				txtrResult_1.setText("");
+				Journeys journeys;
+				if(txtFrom != null && txtTo != null){
+					String searchURL = Constants.getURL(txtFrom.getText(),txtTo.getText(),1);
+					journeys = Parser.getJourneys(searchURL);
+					for (Journey j : journeys.getJourneys()) {
+						txtrResult_1.append(j.getStartStation()+" -> "+j.getEndStation()+"\n");
+						String dep = format(j.getDepDateTime().get(Calendar.HOUR_OF_DAY))+":"+format(j.getDepDateTime().get(Calendar.MINUTE));
+						String arr = format(j.getArrDateTime().get(Calendar.HOUR_OF_DAY))+":"+format(j.getArrDateTime().get(Calendar.MINUTE));
+						txtrResult_1.append("Avgår: "+dep+" -> Anländer: "+arr);
+					}
+				}
+			}
+		});
+		btnSearch_1.setBounds(298, 7, 117, 29);
+		panel_1.add(btnSearch_1);
+		
+		txtrResult_1.setText("Result");
+		txtrResult_1.setBounds(6, 36, 426, 99);
+		panel_1.add(txtrResult_1);
+		
 	}
+	
+	private String format(int time){
+		String strTime;
+		if (time < 10){
+			strTime = "0"+String.valueOf(time);
+		} else {
+			strTime = String.valueOf(time);
+		}
+		return strTime;
+	}
+	
+	private void startThread(String search){
+		txtrResult.setText("...");
+		Thread thread = new SearchThread(search);
+		thread.start();
+	}
+	
+	private void startThread(String from, String to){
+		txtrResult_1.setText("...");
+		Thread thread = new SearchThread(from, to);
+		thread.start();
+	}
+	
+	private void setStationText(){
+		
+	}
+	
+	private void setJourneyText(){
+		
+	}
+	
 }
---
+++ A6_Jesper_Andersson/src/SearchThread.java
@@ -0,0 +1,24 @@
+
+public class SearchThread extends Thread{
+	
+	private String search;
+	private String from;
+	private String to;
+	
+	public SearchThread(String search){
+		this.search = search;
+	}
+	
+	public SearchThread(String from, String to){
+		this.from = from;
+		this.to = to;
+	}
+	
+	public void run(){
+		if(search == null){
+			
+		} else {
+			
+		}
+	}
+}
